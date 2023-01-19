8a7388ff4470329d2d3c30c496102c834a9474d9
thilaks <thilak7c@gmail.com>
2023-01-20 00:24:55 +0800

    second commit

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+import hamburgerMenu from "./hamburgerMenu.svg";
+import close from "./close.svg";
+import logo from "./logo.svg";
+import lock from "./lock.svg"; 
+
+ 
+export {
+  hamburgerMenu,
+  close,
+  logo,
+  lock,
+};
